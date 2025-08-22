7408f74900ee95ab3ae959d312361d1a5421497f
BoldCameleon <gieseckestudent@outlook.de>
2025-08-22 13:05:24 +0200

    1.6 Update mit src Code ohne Prog Fragen

---
+++ AbstrakteKlassen/src/MainAbstractClasses.java
@@ -0,0 +1,11 @@
+public class MainAbstractClasses {
+    public static void main(String[] args) {
+
+        Cat myCat = new Cat(); 
+        myCat.makeNoise();
+
+        myCat.eat();
+
+    }
+}
+
