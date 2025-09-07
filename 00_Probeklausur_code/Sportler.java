d62447a2c470c2f7bdb33e8bc0ed73bc175d9163
nuemit <n4no2n3ti@gmail.com>
2025-09-07 17:23:38 +0200

    hinzufügen von Sportler und Wettkampfteilnehmer

---
+++ 00_Probeklausur_code/Sportler.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class Sportler {
+
+    private String name;
+    private LocalDate geburtsdatum;
+
+    public Sportler() {
+        this(null, null);
+    }
+    public Sportler(String nName, LocalDate gbDatum) {
+        this.setGeburtsDatum(gbDatum);
+        this.setName(nName);
+    }
+
+    public void setName(String nName) {
+        this.name = nName;
+    }
+
+    public void setGeburtsDatum(LocalDate gbDatum) {
+        this.geburtsdatum = gbDatum;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public LocalDate getGeburtsDatum() {
+        return this.geburtsdatum;
+    }
+
+   @Override
+   public String toString() {
+    StringBuffer sb = new StringBuffer();
+    sb.append("Name: ")
+    .append(this.name)
+    .append("\nGeburtsdatum")
+    .append(this.geburtsdatum.toString());
+
+    return sb.toString();
+   } 
+}
---
+++ 00_Probeklausur_code/WettKampfTeilnehmer.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class WettKampfTeilnehmer extends Sportler implements Comparable<WettKampfTeilnehmer> {
+
+    private int startnummer;
+    private static int anzTeilnehmer = 1;
+    
+    public WettKampfTeilnehmer (String nName, LocalDate gbDate, int nStartnummer) {
+        super(nName,gbDate);
+        this.setStartnummer(nStartnummer);
+        anzTeilnehmer++;
+    }
+
+    @Override
+    public int compareTo(WettKampfTeilnehmer o) {
+        return Integer.compare(this.startnummer, o.getStartnummer());
+    }
+
+    public int getAnzahlTeilnehmer() {
+        return anzTeilnehmer;
+    }
+
+    public int getStartnummer() {
+        return this.startnummer;
+    }
+    
+    private void setStartnummer(int nStartnummer) {
+        this.startnummer = Math.abs(nStartnummer);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(super.toString())
+        .append("\nStartnummer: ")
+        .append(this.getStartnummer());
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) 
+            return true;
+
+        if (obj == null || obj.getClass() != this.getClass()) 
+            return false;
+
+        WettKampfTeilnehmer oth = (WettKampfTeilnehmer) obj;
+
+        return this.getStartnummer() == oth.getStartnummer();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.getStartnummer());
+    } 
+}
