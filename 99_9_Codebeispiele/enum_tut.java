d1a8521fa87a3512c3475f3554125a6adf828e72
Br-Eric01 <eric.bronowski@gmail.com>
2026-01-05 12:01:29 +0100

    Kleine Veränderungen an enum_tut.java.

--- 99_9_Codebeispiele/enum_tut.java
+++ 99_9_Codebeispiele/enum_tut.java
@@ -1,11 +1,14 @@
 public class enum_tut {
     public static void main (String Args[]) { 
         // Einzelner Auszug
-        System.out.println(day.MON.guterTag);
+        // System.out.println(day.MON.guterTag);
         
         // Kompletter Auzug von dem Kompletten Enum als Array
         for (day tage: day.values()) {
-            System.out.println(tage);
+            //System.out.println(tage);
+            //System.out.println(tage.guterTag);
+
+            System.out.println(tage + ": " + tage.guterTag);
         }
     }
 }
