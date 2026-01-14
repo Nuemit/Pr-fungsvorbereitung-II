28d9a23965cc1e853fe905a93aa306447339cf33
Br-Eric01 <eric.bronowski@gmail.com>
2026-01-14 13:13:02 +0100

    Kleine Veränderungen.

---
+++ 99_9_Codebeispiele/schmeissen.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class schmeissen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt(); 
+
+        alterTest(age);
+    }   
+
+
+    public static void alterTest (int alter) throws ArithmeticException {
+        if (alter < 18) {
+            throw new ArithmeticException ("Du bist zu jung.");
+        } else { 
+            System.out.println("Du bist alt genug.");
+        }
+    }
+}
