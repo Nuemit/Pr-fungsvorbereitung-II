f95b2b2d428443d94997a18812de2cc0cc94f109
Br-Eric01 <eric.bronowski@gmail.com>
2026-01-15 14:37:14 +0100

    Scanner geschlossen.

--- 99_9_Codebeispiele/schmeissen.java
+++ 99_9_Codebeispiele/schmeissen.java
@@ -4,7 +4,7 @@ public class schmeissen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt(); 
-
+        sc.close();
         alterTest(age);
     }   
 
